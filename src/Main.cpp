d3f8c18bfc32d0c947f8871d547c30e56a1a6b37
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-24 17:43:26 +0000

    Switching to magnum + sdl.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,36 +1,78 @@
-#include <iostream>
+#include <Magnum/GL/Buffer.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/GL/Renderer.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Math/Matrix4.h>
+#include <Magnum/MeshTools/CompressIndices.h>
+#include <Magnum/MeshTools/Interleave.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Primitives/Cube.h>
+#include <Magnum/Shaders/Phong.h>
+#include <Magnum/Trade/MeshData.h>
 
-#include <GLFW/glfw3.h>
+using namespace Magnum;
+using namespace Magnum::Math::Literals;
 
-int main() {
-  GLFWwindow *window;
+class Raytrace : public Platform::Application {
+public:
+  explicit Raytrace(const Arguments &arguments);
 
-  /* Initialize the library */
-  if (!glfwInit())
-    return -1;
+private:
+  void drawEvent() override;
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello Raytrace", NULL, NULL);
-  if (!window) {
-    glfwTerminate();
-    return -1;
-  }
+  GL::Mesh _mesh;
+  Shaders::Phong _shader;
 
-  /* Make the window's context current */
-  glfwMakeContextCurrent(window);
+  Matrix4 _transformation, _projection;
+  Color3 _color;
+};
 
-  /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window)) {
-    /* Render here */
-    glClear(GL_COLOR_BUFFER_BIT);
+Raytrace::Raytrace(const Arguments &arguments)
+    : Platform::Application{
+          arguments, Configuration{}.setTitle("Raytrace")} {
+  GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+  GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
 
-    /* Swap front and back buffers */
-    glfwSwapBuffers(window);
+  Trade::MeshData cube = Primitives::cubeSolid();
 
-    /* Poll for and process events */
-    glfwPollEvents();
-  }
+  GL::Buffer vertices;
+  vertices.setData(
+      MeshTools::interleave(cube.positions3DAsArray(), cube.normalsAsArray()));
 
-  glfwTerminate();
-  return 0;
+  std::pair<Containers::Array<char>, MeshIndexType> compressed =
+      MeshTools::compressIndices(cube.indicesAsArray());
+  GL::Buffer indices;
+  indices.setData(compressed.first);
+
+  _mesh.setPrimitive(cube.primitive())
+      .setCount(cube.indexCount())
+      .addVertexBuffer(std::move(vertices), 0, Shaders::Phong::Position{},
+                       Shaders::Phong::Normal{})
+      .setIndexBuffer(std::move(indices), 0, compressed.second);
+
+  _transformation =
+      Matrix4::rotationX(30.0_degf) * Matrix4::rotationY(40.0_degf);
+  _projection =
+      Matrix4::perspectiveProjection(
+          35.0_degf, Vector2{windowSize()}.aspectRatio(), 0.01f, 100.0f) *
+      Matrix4::translation(Vector3::zAxis(-10.0f));
+  _color = Color3::fromHsv({35.0_degf, 1.0f, 1.0f});
 }
+
+void Raytrace::drawEvent() {
+  GL::defaultFramebuffer.clear(GL::FramebufferClear::Color |
+                               GL::FramebufferClear::Depth);
+
+  _shader.setLightPositions({{7.0f, 5.0f, 2.5f, 0.0f}})
+      .setDiffuseColor(_color)
+      .setAmbientColor(Color3::fromHsv({_color.hue(), 1.0f, 0.3f}))
+      .setTransformationMatrix(_transformation)
+      .setNormalMatrix(_transformation.normalMatrix())
+      .setProjectionMatrix(_projection)
+      .draw(_mesh);
+
+  swapBuffers();
+}
+
+MAGNUM_APPLICATION_MAIN(Raytrace)
