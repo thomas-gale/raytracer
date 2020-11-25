ddc70af8c372e6465d59b59382e3d099b91797b7
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-25 20:30:26 +0000

    Adding Vec3 with utility functions.

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,105 +2,110 @@
 
 #include <SDL.h>
 
+#include "Vec3.hpp"
+
 int main() {
-  std::cout << "Hello Raytrace" << std::endl;
+    std::cout << "Hello Raytrace" << std::endl;
 
-  // Settings.
-  int width = 640;
-  int height = 480;
+    // Settings.
+    int width = 640;
+    int height = 480;
 
-  // Start SDL
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-    return 1;
-  }
-
-  // Open a window
-  SDL_Window *win = SDL_CreateWindow("Hello World!", 1200, 100, width, height,
-                                     SDL_WINDOW_SHOWN);
-  if (win == nullptr) {
-    std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-    SDL_Quit();
-    return 1;
-  }
-
-  // Creating a renderer
-  SDL_Renderer *ren = SDL_CreateRenderer(
-      win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  if (ren == nullptr) {
-    SDL_DestroyWindow(win);
-    std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-    SDL_Quit();
-    return 1;
-  }
-
-  // Create my own surface.
-  SDL_Surface *surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
-
-  // Fiddle with the pixels
-  SDL_LockSurface(surface);
-
-  // Set fixed colour (white)
-  Uint32 color = 0x00FF00;
-  for (int y = 0; y < height; ++y) {
-    std::cerr << "\rScanlines remaining: " << height - y - 1 << " " << std::flush;
-    for (int x = 0; x < width; ++x) {
-      // Compute color
-      auto r = double(x) / double(width);
-      auto g = double(height-y) / double(height);
-      auto b = 0.25;
-
-      Uint32 ir = static_cast<Uint8>(255.999 * r);
-      Uint32 ig = static_cast<Uint8>(255.999 * g);
-      Uint32 ib = static_cast<Uint8>(255.999 * b);
-
-      // Grab the pixel pointer
-      Uint8 *pixel = (Uint8 *)surface->pixels;
-      pixel += (y * surface->pitch) + (x * sizeof(Uint32));
-
-      // Compose and set the color
-      ir <<= 16;
-      ig <<= 8;
-      Uint32 c = ir | ig | ib;
-      *((Uint32 *)pixel) = c;
+    // Start SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
     }
-  }
 
-  SDL_UnlockSurface(surface);
+    // Open a window
+    SDL_Window* win = SDL_CreateWindow("Hello World!", 1200, 100, width, height,
+                                       SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
 
-  // Create texture from surface
-  SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, surface);
+    // Creating a renderer
+    SDL_Renderer* ren = SDL_CreateRenderer(
+        win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError()
+                  << std::endl;
+        SDL_Quit();
+        return 1;
+    }
 
-  // NOTE - surface freed.
-  SDL_FreeSurface(surface);
-  if (tex == nullptr) {
+    // Create my own surface.
+    SDL_Surface* surface =
+        SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+
+    // Fiddle with the pixels
+    SDL_LockSurface(surface);
+
+    // Set fixed colour (white)
+    Uint32 color = 0x00FF00;
+    for (int y = 0; y < height; ++y) {
+        std::cerr << "\rScanlines remaining: " << height - y - 1 << " "
+                  << std::flush;
+        for (int x = 0; x < width; ++x) {
+            // Compute color
+            auto r = double(x) / double(width);
+            auto g = double(height - y) / double(height);
+            auto b = 0.25;
+
+            Uint32 ir = static_cast<Uint8>(255.999 * r);
+            Uint32 ig = static_cast<Uint8>(255.999 * g);
+            Uint32 ib = static_cast<Uint8>(255.999 * b);
+
+            // Grab the pixel pointer
+            Uint8* pixel = (Uint8*)surface->pixels;
+            pixel += (y * surface->pitch) + (x * sizeof(Uint32));
+
+            // Compose and set the color
+            ir <<= 16;
+            ig <<= 8;
+            Uint32 c = ir | ig | ib;
+            *((Uint32*)pixel) = c;
+        }
+    }
+
+    SDL_UnlockSurface(surface);
+
+    // Create texture from surface
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, surface);
+
+    // NOTE - surface freed.
+    SDL_FreeSurface(surface);
+    if (tex == nullptr) {
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError()
+                  << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Drawing the texture.
+    // A sleepy rendering loop, wait for 3 seconds and render and present the
+    // screen each time
+    for (int i = 0; i < 3; ++i) {
+        // First clear the renderer
+        SDL_RenderClear(ren);
+        // Draw the texture
+        SDL_RenderCopy(ren, tex, NULL, NULL);
+        // Update the screen
+        SDL_RenderPresent(ren);
+        // Take a quick break after all that hard work
+        SDL_Delay(1000);
+    }
+
+    // Clean up
+    SDL_DestroyTexture(tex);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
-    std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError()
-              << std::endl;
     SDL_Quit();
-    return 1;
-  }
 
-  // Drawing the texture.
-  // A sleepy rendering loop, wait for 3 seconds and render and present the
-  // screen each time
-  for (int i = 0; i < 3; ++i) {
-    // First clear the renderer
-    SDL_RenderClear(ren);
-    // Draw the texture
-    SDL_RenderCopy(ren, tex, NULL, NULL);
-    // Update the screen
-    SDL_RenderPresent(ren);
-    // Take a quick break after all that hard work
-    SDL_Delay(1000);
-  }
-
-  // Clean up
-  SDL_DestroyTexture(tex);
-  SDL_DestroyRenderer(ren);
-  SDL_DestroyWindow(win);
-  SDL_Quit();
-
-  return 0;
+    return 0;
 }
---
+++ src/Vec3.hpp
@@ -0,0 +1,82 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+template <class T> class Vec3 {
+  public:
+    Vec3() : e{0.0, 0.0, 0.0} {}
+    Vec3(T x, T y, T z) : e{x, y, z} {}
+
+    T x() const { return e[0]; }
+    T y() const { return e[1]; }
+    T z() const { return e[2]; }
+
+    // Primary Functions.
+
+    Vec3 operator-() const { return Vec3(-e[0], -e[1], -e[2]); }
+    T operator[](int i) const { return e[i]; }
+    T& operator[](int i) { return e[i]; }
+
+    Vec3& operator+=(const Vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    Vec3& operator*=(const T t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    Vec3& operator/=(const T t) { return *this *= 1.0 / t; }
+
+    T length() const { return std::sqrt<T>(lengthSquared()); }
+
+    T lengthSquared() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+
+    // Utility functions
+
+    inline friend std::ostream& operator<<(std::ostream& out, const Vec3& v) {
+        return out << v.e[0] << " " << v.e[1] << " " << v.e[2];
+    }
+
+    inline friend Vec3 operator+(const Vec3& u, const Vec3& v) {
+        return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+    }
+
+    inline friend Vec3 operator-(const Vec3& u, const Vec3& v) {
+        return Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+    }
+
+    inline friend Vec3 operator*(const Vec3& u, const Vec3& v) {
+        return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+    }
+
+    inline friend Vec3 operator*(T t, const Vec3& v) {
+        return Vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+    }
+
+    inline friend Vec3 operator*(const Vec3& v, T t) { return t * v; }
+
+    inline friend Vec3 operator/(const Vec3& v, T t) { return (1.0 / t) * v; }
+
+    inline friend T dot(const Vec3& u, const Vec3& v) {
+        return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+    }
+
+    inline friend Vec3 cross(const Vec3& u, const Vec3& v) {
+        return Vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                    u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                    u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+    }
+
+  private:
+    T e[3];
+};
+
+#endif // VEC3_H
