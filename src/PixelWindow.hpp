54d2cf186fac98b45dbec69fef123f0bf69c3ee1
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-27 19:12:45 +0000

    Cleaning up the pretty draw color.

--- src/PixelWindow.hpp
+++ src/PixelWindow.hpp
@@ -37,17 +37,7 @@ template <class T> class PixelWindow {
             return;
         }
 
-        // Create my own surface.
-        // surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
-
-        // setPrettyColor();
-
-        // Create texture from surface
-        // tex = SDL_CreateTextureFromSurface(ren, surface);
-
-        // NOTE - surface freed.
-        // SDL_FreeSurface(surfacae);
-
+        // Create the texture
         tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGBA8888,
                                 SDL_TEXTUREACCESS_STREAMING, width, height);
         if (tex == nullptr) {
@@ -59,20 +49,15 @@ template <class T> class PixelWindow {
             return;
         }
 
-        pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
-
-        // Grissly :D
-        pixels = (uint32_t*)malloc(sizeof(uint32_t) * width * height);
-
-        SDL_SetRenderDrawColor(ren, 225, 240, 200, 255);
+        // Init low level pixel storage
+        //pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
+        //pixels = (uint32_t*)malloc(sizeof(uint32_t) * width * height);
+        //SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
     }
 
     ~PixelWindow() {
-        // SDL_FreeSurface(surface);
-
-        free(pixelFormat);
-        free(pixels);
-
+        //free(pixelFormat);
+        //free(pixels);
         SDL_DestroyTexture(tex);
         SDL_DestroyRenderer(ren);
         SDL_DestroyWindow(win);
@@ -80,33 +65,6 @@ template <class T> class PixelWindow {
     }
 
     void draw() {
-
-        // Uint32 format;
-        // int w;
-        // int h;
-        // SDL_QueryTexture(tex, &format, NULL, &w, &h);
-
-        /*int pitch;
-        uint8_t* pixels;
-        SDL_LockTexture(tex, NULL, (void**)&pixels, &pitch);
-        for (int y = 0; y < height; y++) {
-            Uint32* p =
-                (Uint32*)(pixels + pitch * y); // cast for a pointer increments
-                                               // by 4 bytes.(RGBA)
-            for (int x = 0; x < width; x++) {
-                // *p = 0x00FF0000;
-                if (x > 20)
-                    *p = SDL_MapRGBA(pixelFormat, 255, 0, 0, 255);
-                else if (x > 10)
-                    *p = SDL_MapRGBA(pixelFormat, 0, 255, 0, 255);
-                else
-                    *p = SDL_MapRGBA(pixelFormat, 0, 0, 255, 255);
-
-                p++;
-            }
-        }
-        SDL_UnlockTexture(tex);*/
-
         SDL_RenderClear(ren);
         SDL_RenderCopy(ren, tex, NULL, NULL);
         SDL_RenderPresent(ren);
@@ -135,68 +93,34 @@ template <class T> class PixelWindow {
         SDL_UnlockTexture(tex);
     }
 
-    /*void setPrettyColor() {
-        // Fiddle with the pixels
-        SDL_LockSurface(surface);
-
-        // Set fixed colour (white)
-        for (int y = 0; y < height; ++y) {
+    void setSolidColor(const Color<T>& color) {
+        int pitch;
+        uint8_t* pixels;
+        SDL_LockTexture(tex, NULL, (void**)&pixels, &pitch);
+        for (int y = 0; y < height; y++) {
             std::cerr << "\rScanlines remaining: " << height - y - 1 << " "
                       << std::flush;
-            for (int x = 0; x < width; ++x) {
-                // Compute color
-                auto r = double(x) / double(width);
-                auto g = double(height - y) / double(height);
-                auto b = 0.25;
-
-                // Grab the pixel pointer
-                Uint8* pixel = (Uint8*)surface->pixels;
-                pixel += (y * surface->pitch) + (x * sizeof(Uint32));
-
-                // Compose and set the color
-                Uint32 c = convertColor(Color<double>(r, g, b));
-                *((Uint32*)pixel) = c;
+            Uint32* p =
+                (Uint32*)(pixels + pitch * y); // cast for a pointer increments
+                                               // by 4 bytes.(RGBA)
+            for (int x = 0; x < width; x++) {
+                *p = convertRGBA(color);
+                ++p;
             }
         }
+        SDL_UnlockTexture(tex);
+    }
 
-        SDL_UnlockSurface(surface);
-    }*/
-
-    /*void setSolidColor(const Color<T>& color) {
-        // Fiddle with the pixels
-        SDL_LockSurface(surface);
-
-        // Set fixed colour (white)
-        for (int y = 0; y < height; ++y) {
-            std::cerr << "\rScanlines remaining: " << height - y - 1 << " "
-                      << std::flush;
-            for (int x = 0; x < width; ++x) {
-                // Grab the pixel pointer
-                Uint8* pixel = (Uint8*)surface->pixels;
-                pixel += (y * surface->pitch) + (x * sizeof(Uint32));
-
-                Uint32 c = convertColor(color);
-                *((Uint32*)pixel) = c;
-            }
-        }
-
-        SDL_UnlockSurface(surface);
-    }*/
-
-    /*void setPixel(int x, int y, const Color<T>& color) {
-        // Fiddle with the pixels
-        SDL_LockSurface(surface);
-
-        // Grab the pixel pointer
-        Uint8* pixel = (Uint8*)surface->pixels;
-        pixel += (y * surface->pitch) + (x * sizeof(Uint32));
-
-        // Compose and set the color
-        Uint32 c = convertColor(color);
-        *((Uint32*)pixel) = c;
-
-        SDL_UnlockSurface(surface);
-    }*/
+    void setPixel(int x, int y, const Color<T>& color) {
+        int pitch;
+        uint8_t* pixels;
+        SDL_LockTexture(tex, NULL, (void**)&pixels, &pitch);
+        Uint32* p = (Uint32*)(pixels + pitch * y +
+                              pitch * x); // cast for a pointer increments
+                                          // by 4 bytes.(RGBA)
+        *p = convertRGBA(color);
+        SDL_UnlockTexture(tex);
+    }
 
     PixelWindow(const PixelWindow& other) = delete;
     PixelWindow(PixelWindow&& other) = delete;
@@ -208,11 +132,8 @@ template <class T> class PixelWindow {
     SDL_Window* win;
     SDL_Renderer* ren;
     SDL_Texture* tex;
-    uint32_t* pixels;
-    SDL_PixelFormat* pixelFormat;
-
-    // To remove
-    // SDL_Surface* surface;
+    //uint32_t* pixels;
+    //SDL_PixelFormat* pixelFormat;
 };
 
 } // namespace raytrace
